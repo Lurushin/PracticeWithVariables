d139d8484f6f7fe28fb5311182c65776a8cd913e
Lurushin <luke.rushing@allsaintshigh.org>
2024-08-29 11:20:29 -0500

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,39 @@
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+        byte byteOne = 64;
+        short shortOne = 200;
+        int intOne = 4000000;
+        long longOne = 700000000;
+        float floatOne = 3.1416f;
+        double doubleOne = 3.14159;
+        char charOne = 65;
+        boolean booleanOne = false;
+        String stringOne = "A";
+        System.out.println("/////////////////////////////////////");
+        System.out.println("*Luke Rushing               08/29/24*");
+        System.out.println("*                                   *");
+        System.out.println("*           integer types           *");
+        System.out.println("*                                   *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"               *");
+        System.out.println("*16 bit - shortOne = "+shortOne+"            *");
+        System.out.println("*32 bit - intOne = "+intOne+"          *");
+        System.out.println("*64 bit - longOne = "+longOne+"       *");
+        System.out.println("*                                   *");
+        System.out.println("*            real types             *");
+        System.out.println("*                                   *");
+        System.out.println("*32 bit - floatOne = "+floatOne+"         *");
+        System.out.println("*64 bit - doubleOne = "+doubleOne+"       *");
+        System.out.println("*                                   *");
+        System.out.println("*        other integer types        *");
+        System.out.println("*                                   *");
+        System.out.println("*16 bit - charOne = "+charOne+"               *");
+        System.out.println("*                                   *");
+        System.out.println("*            other types            *");
+        System.out.println("*                                   *");
+        System.out.println("*booleanOne = "+booleanOne+"                 *");
+        System.out.println("*stringOne = "+stringOne+"                      *");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
